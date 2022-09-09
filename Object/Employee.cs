91d98505a109e141f2a9e59cdc860a1931f090aa
Hamse20 <57354156+Hamse20@users.noreply.github.com>
2022-09-09 13:02:22 +0200

    Add project files.

---
+++ Object/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+    internal class Employee
+
+    {
+        public string? name;
+        public int lön; 
+
+        public void CountSalary()
+        {
+           Console.WriteLine(lön + (lön *.10));
+        }
+    }
+}
---
+++ Object/Program.cs
@@ -0,0 +1,19 @@
+﻿using Object;
+
+Employee lasse = new Employee();
+
+lasse.name = "Lasse Andersson";
+lasse.lön = 35000;
+lasse.CountSalary();
+
+Console.WriteLine(lasse.name + "har " +lasse.lön );
+
+Console.WriteLine("Hellow World");
+
+Console.ReadKey();
+
+
+
+
+
+
